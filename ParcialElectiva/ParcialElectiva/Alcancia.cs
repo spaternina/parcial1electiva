3ca8a08a3ebb2b0509dee0427950fdbc0b46aa9c
Sebastian Paternina <spaterninachavez@gmail.com>
2022-03-09 21:54:21 -0500

    Primer avance

---
+++ ParcialElectiva/ParcialElectiva/Alcancia.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParcialElectiva
+{
+    internal class Alcancia
+    {
+        Dictionary<int, int> monedas = new Dictionary<int, int>();
+        public Alcancia()
+        {
+            monedas.Add(50, 0);
+            monedas.Add(100, 0);
+            monedas.Add(200, 0);
+            monedas.Add(500, 0);
+            monedas.Add(1000, 0);
+        }
+        
+
+        public bool InsertarDinero()
+        {
+            int opcion;
+            Console.WriteLine("\nIngrese la moneda a insertar" +
+                "\n1) 50" +
+                "\n2) 100" +
+                "\n3) 200" +
+                "\n4) 500" +
+                "\n5) 1000");
+            opcion = Convert.ToInt32(Console.ReadLine());
+
+            int monedaElegida;
+            switch(opcion)
+            {
+                case 1:
+                    monedaElegida = 50;
+                    break;
+                case 2:
+                    monedaElegida = 100;
+                    break;
+                case 3:
+                    monedaElegida = 200;
+                    break;
+                case 4:
+                    monedaElegida = 500;
+                    break;
+                case 5:
+                    monedaElegida = 1000;
+                    break;
+                default:
+                    monedaElegida = 0;
+                    break;
+            }
+
+            if (monedaElegida == 0)
+            {
+                return false;
+            }
+
+            Console.WriteLine("\nIngrese la cantidad de monedas.");
+            int cantidad = Convert.ToInt32(Console.ReadLine());
+            monedas[monedaElegida] += cantidad;
+            Console.WriteLine("\nDinero ingresado con éxito.");
+            return true;
+        }
+
+        public void ImprimirMoneda()
+        {
+            int opcion;
+            Console.WriteLine("\nIngrese la moneda a consultar" +
+                "\n1) 50" +
+                "\n2) 100" +
+                "\n3) 200" +
+                "\n4) 500" +
+                "\n5) 1000");
+            opcion = Convert.ToInt32(Console.ReadLine());
+
+            int monedaElegida;
+            switch (opcion)
+            {
+                case 1:
+                    monedaElegida = 50;
+                    break;
+                case 2:
+                    monedaElegida = 100;
+                    break;
+                case 3:
+                    monedaElegida = 200;
+                    break;
+                case 4:
+                    monedaElegida = 500;
+                    break;
+                case 5:
+                    monedaElegida = 1000;
+                    break;
+                default:
+                    monedaElegida = 0;
+                    break;
+            }
+
+            Console.WriteLine("\nDinero ingresado con éxito.");
+            Console.WriteLine("Hay " + this.monedas[monedaElegida] + " monedas de este tipo");
+        }
+
+        public void ImprimirTotalPorMoneda()
+        {
+
+        }
+    }
+}
---
+++ ParcialElectiva/ParcialElectiva/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+namespace ParcialElectiva
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			Alcancia alcancia = new Alcancia();
+			int respuesta;
+			do
+			{
+				Console.WriteLine("\n -- Menú --" +
+					"\n1) Ingresar dinero" +
+					"\n2) Ver cantidad por denominación" +
+					"\n3) Ver dinero total por denominación" +
+					"\n4) Ver cantidad de monedas en total" +
+					"\n5) Ver total de dinero" +
+					"\n -- Ingrese opcion diferente a las anteriores para salir --");
+				respuesta = Convert.ToInt32(Console.ReadLine());
+
+				switch (respuesta)
+                {
+					case 1:
+						bool dineroIngresado = alcancia.InsertarDinero();
+						break;
+					case 2:
+						alcancia.ImprimirMoneda();
+						break;
+					case 3:
+						alcancia.ImprimirTotalPorMoneda();
+						break;
+					case 4:
+						alcancia.ImprimirTotalPorMoneda();
+						break;
+				}
+			} while (respuesta >= 1 && respuesta <= 4);
+		}
+	}
+}
